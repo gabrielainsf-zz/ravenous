c03e1c1c5b5f735da802c0a2136f60c1f536a1b9
Gabriela Contreras-Cisneros <gabriela-c@gabrielas-air-2.gateway.sonic.net>
2018-10-08 18:39:47 -0700

    Added functionality to BusinessList component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,15 +3,15 @@ import './BusinessList.css';
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
+
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {
+                    this.props.businesses.map(business => { 
+                        return (<Business business={business} />);
+                })
+            }
             </div>
         );
     }
