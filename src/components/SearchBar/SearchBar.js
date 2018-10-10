6dab22433e9185788b04f4405a361eeb7e88bd7d
Gabriela Contreras-Cisneros <gabriela-c@gabrielas-air-2.gateway.sonic.net>
2018-10-09 21:29:49 -0700

    Simulated search query

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,73 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-}
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match'
+        }
+
+        this.handleSortByChange = this.handleSortByChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+    }
+
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        } else {
+            return '';
+        };
+    }
+
+    handleSortByChange(sortByOption) {
+        this.setState({
+            sortBy: sortByOption
+        });
+    }
+
+    handleTermChange(e) {
+        this.setState({
+            term: e.target.value
+        });
+    }
+
+    handleLocationChange(e) {
+        this.setState({
+            location: e.target.value
+        });
+    }
+
+    handleSearch(e) {
+        this.props.searchYelp(
+            this.state.term,
+            this.state.location,
+            this.state.sortBy);
+        e.preventDefault();
+    }
+
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key={sortByOptionValue}>{sortByOption}</li>;
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            let getSortByClassValue = this.getSortByClass(sortByOptionValue);
+            return <li
+                        key={sortByOptionValue}
+                        className={getSortByClassValue}
+                        onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+                    >
+                        {sortByOption}
+                    </li>;
         });
     }
 
@@ -20,15 +76,15 @@ class SearchBar extends React.Component {
             <div className="SearchBar">
               <div className="SearchBar-sort-options">
                 <ul>
-                  {this.renderSortByOptions}>
+                  {this.renderSortByOptions()}
                 </ul>
               </div>
               <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
+                <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                <input placeholder="Where?" onChange={this.handleLocationChange} />
               </div>
               <div className="SearchBar-submit">
-                <a>Let's Go</a>
+                <a onClick={this.handleSearch}>Let's Go</a>
               </div>
             </div>
         );
